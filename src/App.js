542ec4b2117534ac1dd33475b4259815451060d3
Soham <soham.rane2207@gmail.com>
2022-04-27 10:33:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import logo1 from './download.jfif';
+import {useState} from 'react';
 
+  
+function Item1(prop)
+{
+  
+  return(
+<div>
+        <img src={prop.img}></img>
+        <h2>{prop.label}</h2>
+        <h2>{prop.price}$</h2>
+        <button >Add to Cart</button>
+        </div>
+  );
+}
 function App() {
+  const [count,setcount]=useState(0)
+  const [bill,setbill]=useState(0)
+function shop()
+{
+  setcount(count+1)
+  setbill(bill+500)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <td><Item1 img={logo} label="React" price="200"/></td>
+          <td><Item1 img={logo1} label="Bottle" price="500"/></td>
+          <td><Item1 img={logo1} label="Shoes" price="500"/></td>
+          <td><Item1 img={logo1} label="Toys" price="100"/></td>
+        </tr>
+       
+      </table>
+      <button onClick={shop}> Submit</button>
+<h2>No of Items Perchased:{count}</h2>
+<h2>Total Bill:{bill}</h2>
     </div>
   );
 }
---
+++ src/item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function item(){
+    return(
+        <div>
+        <h2>Bottle</h2>
+        <h2>Price=400$</h2>
+        </div>
+    );
+}
+export default item;
